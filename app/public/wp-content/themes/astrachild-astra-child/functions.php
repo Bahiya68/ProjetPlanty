e848029f3f278142feb606002bf48fccd4dc9b7b
Bahiya68 <bahiya.atouioual@laposte.net>
2023-12-17 00:54:56 +0100

    Creation fichier theme.css

--- app/public/wp-content/themes/astrachild-astra-child/functions.php
+++ app/public/wp-content/themes/astrachild-astra-child/functions.php
@@ -7,5 +7,6 @@ if ( ! function_exists( 'b7ectg_theme_enqueue_styles' ) ) {
     function b7ectg_theme_enqueue_styles() {
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
         wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+        wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     }
 }
