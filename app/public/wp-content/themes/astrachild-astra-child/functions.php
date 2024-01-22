8a71032fa8e7c11d1dede372453fa260303e46b2
Bahiya <bahiya.atouioual@laposte.net>
2024-01-22 10:10:14 +0100

    Modification apres soutenance

--- app/public/wp-content/themes/astrachild-astra-child/functions.php
+++ app/public/wp-content/themes/astrachild-astra-child/functions.php
@@ -15,25 +15,11 @@ if (!function_exists('b7ectg_theme_enqueue_styles')) {
 
 
 
-add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
-  function add_extra_item_to_nav_menu( $items, $args ) {
-      if (is_user_logged_in() && ($args)) {
-          $items .= '<li class="menu-item menu-item-type-post_type "><a class="menu-link" href="'.  get_admin_url() .'">Admin</a></li>';
-     }
-
-     return $items;
-  
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2);
+function add_extra_item_to_nav_menu($items, $args)
+{
+    if (is_user_logged_in() && ($args)) {
+        $items .= '<li class="menu-item menu-item-type-post_type "><a class="menu-link" href="' .  get_admin_url() . '">Admin</a></li>';
     }
-
-
-
-
-
- 
-
-   
-
-
-
-
-
+    return $items;
+}
